41fa527d4626e82d7874b88a21b8feb2dc96cd7b
Martin Wedepohl <martin.wedepohl@shaw.ca>
2020-01-18 23:31:14 -0800

    0.1.6 Added instructions on how to use the plugin Updated the language pot file

--- bisk-options-plugin.php
+++ bisk-options-plugin.php
@@ -7,7 +7,7 @@
  * types and shortcodes required by the BISK website.
  * Author: Martin Wedepohl <martin@wedepohlengineering.com>
  * Author URI: https://wedepohlengineering.com
- * Version: 0.1.5
+ * Version: 0.1.6
  * Text Domain: bisk-options-plugin
  */
 /*
